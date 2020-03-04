5b611d2087b685a247ea162f86cec3963246c843
Nikita Parkhomenko <nikita.parkhom@gmail.com>
2020-03-04 17:11:36 +0200

    Added photo user name info into ImageItem component

--- src/components/image-item/image-item.jsx
+++ src/components/image-item/image-item.jsx
@@ -5,6 +5,7 @@ import './image-item.css';
 const ImageItem = ({ img, clicked }) => (
     <div className="image-item" onClick={clicked} >
         <img alt={img.alt_description} src={img.urls.thumb} />
+        <span> User Name: {img.user.username} </span>
         <span> Author: {img.user.name} </span>
     </div>
 );
